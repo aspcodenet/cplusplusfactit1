b7bc095389300297ad6fc905dd472f53e20151de
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-10-10 11:10:06 +0200

    f2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main(){
-    cout << "Hello" << endl;
+    // cout << "Hello my name is Stefan" << endl;
+    // cout << "Testgatan 12" << endl;
+    // cout << "111 22  Teststad" << endl;
+
+    // 12 + 15
+    // 12 + 15 + 99
+    cout    << "Hello my name is Stefan" << endl   
+            << "Testgatan 12" << endl 
+            << "111 22  Teststad" << endl;
+
     return 0;
 }
